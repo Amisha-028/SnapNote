286062f58d2cf1ee44484e650b3342da60327c63
amisha-028 <iamisha0018@gmail.com>
2024-06-10 21:46:02 +0530

    added store

---
+++ SnapNote/src/conf/conf.js
@@ -0,0 +1,11 @@
+// yaha pe hum bhot sare key value ko store karege
+const conf={
+    // it will ensure that this will always give value in string not in integer
+    appwriteUrl : String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId : String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId : String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId : String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId : String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+}
+
+export default conf;
